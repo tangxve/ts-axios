9594e625f2e43f9b667e51c087ce356f3a2a0d49
tangxue <11183504@qq.com>
2021-08-18 09:59:19 +0800

    feat: 更新 url 方法

---
+++ src/helpers/url.ts
@@ -0,0 +1,32 @@
+import { isDate, isObject } from './util'
+
+export function buildURL(url: string, params: string): string {
+  if (!params) return url
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+
+    let values = []
+
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+    })
+  })
+}
