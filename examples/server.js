75ad3f44fdecd8e355a571c0b3c95067dd68123c
tangxue <11183504@qq.com>
2021-08-18 09:59:19 +0800

    feat: 更新 url 方法

--- examples/server.js
+++ examples/server.js
@@ -59,7 +59,7 @@ registerMoreRouter()
 
 app.use(router)
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8070
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,22 @@
 export type Method =
-  'get' | 'GET' |
-  'delete' | 'Delete' |
-  'head' | 'HEAD' |
-  'options' | 'OPTIONS' |
-  'post' | 'POST' |
-  'put' | 'PUT' |
-  'patch' | 'PATCH'
+  | 'get'
+  | 'GET'
+  | 'put'
+  | 'PUT'
+  | 'head'
+  | 'HEAD'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
+  | 'delete'
+  | 'Delete'
+  | 'options'
+  | 'OPTIONS'
 
 export interface AxiosRequestConfig {
-  url: string,
-  method?: Method,
-  data?: any,
+  url: string
+  method?: Method
+  data?: any
   params?: any
 }
