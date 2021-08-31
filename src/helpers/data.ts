5070c63b7105021da34ff0afb8709a8b81bdccba
tangxue <11183504@qq.com>
2021-08-31 15:09:37 +0800

    feat: 处理响应头 data 数据

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,8 +1,23 @@
 import { isPlainObject } from './util'
 
+// 转换请求的 data
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
     return JSON.stringify(data)
+  }
+
+  return data
+}
+
+// 转换响应的 data
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    // 报错兼容
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // do nothing
+    }
   }
 
   return data
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { AxiosPromise, AxiosRequestConfig } from './types'
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/header'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
@@ -9,7 +9,7 @@ function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
 
   // 发送请求
-  return xhr(config)
+  return xhr(config).then(res => transformResponseData(res))
 }
 
 // 发送 xhr 之前对 config 做处理 void 没有任何类型
@@ -27,17 +27,24 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildURL(url, params)
 }
 
-// 处理请求 data
+// 处理请求头 body 数据
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
-// 处理 headers
+// 处理请求头 headers
 function transformHeaders(config: AxiosRequestConfig): any {
   // headers 要给个默认值，处理header方法判断了header
   const { headers = {}, data } = config
 
   return processHeaders(headers, data)
+}
+
+// 处理响应头 data
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+
+  return res
 }
 
 export default axios
