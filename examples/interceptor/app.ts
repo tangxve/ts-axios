de0bd8684c0135299aa8ea3a2b061c2c0f5370f3
tangxue <11183504@qq.com>
2021-09-22 13:38:33 +0800

    feat(梳理链式调用流程):

--- examples/interceptor/app.ts
+++ examples/interceptor/app.ts
@@ -2,14 +2,17 @@ import axios from '../../src/index'
 
 /* 请求拦截器 */
 axios.interceptors.request.use(config => {
+  console.log(1)
   config.headers.test += '1'
   return config
 })
 axios.interceptors.request.use(config => {
+  console.log(2)
   config.headers.test += '2'
   return config
 })
 axios.interceptors.request.use(config => {
+  console.log(3)
   config.headers.test += '3'
   return config
 })
@@ -20,17 +23,17 @@ axios.interceptors.response.use(res => {
   res.data += '1'
   return res
 })
-let interceptor = axios.interceptors.response.use(res => {
-  res.data += '2'
-  return res
-})
+// let interceptor = axios.interceptors.response.use(res => {
+//   res.data += '2'
+//   return res
+// })
 axios.interceptors.response.use(res => {
   res.data += '3'
   return res
 })
 
 /* 删除拦截器 */
-axios.interceptors.response.eject(interceptor)
+// axios.interceptors.response.eject(interceptor)
 
 axios({
   url: '/interceptor/get',
